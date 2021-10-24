df080ebb737140729e2563710c14dbdc293cbd0d
Gastón Michel <michel.z.gaston@gmail.com>
2021-10-24 17:27:37 -0300

    lambda placeholders

---
+++ lambda.py
@@ -0,0 +1,10 @@
+def getBookById(event, context):
+    pass
+def listBooks(event, context):
+    pass
+def createBook(event, context):
+    pass
+def deleteBook(event, context):
+    pass
+def updateBook(event, context):
+    pass
